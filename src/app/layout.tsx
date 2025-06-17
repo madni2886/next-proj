f93bb9e48d706cb4622490c7ec86f79d0537a277
Muhammad Madni <muhammad.madni@nxb.com.pk>
2025-06-17 12:02:37 +0500

    refix devs last

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 // const geistSans = Geist({
