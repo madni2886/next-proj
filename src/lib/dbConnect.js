426b0c4d9ff48d7797bb03d06fb873ec9d0f9f18
Muhammad Madni <muhammad.madni@nxb.com.pk>
2025-06-17 13:58:52 +0500

    refix mongodb uri01

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
+console.log('MONGODB_URI:', MONGODB_URI);
+
+if (!MONGODB_URI) {
+  
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+mongoose.set('strictQuery', false);
 
 const dbConnect = async () => {
   if (mongoose.connection.readyState >= 1) return;
